f9e6f741bbd84786d28fb0b0e9ede8b99c31b682
Jake Morrison <jake@cogini.com>
2018-06-15 13:18:27 +0800

    Add rocksdb

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule FileConfig.Mixfile do
       # {:recon, github: "ferd/recon", override: true},
       {:nimble_csv, "~> 0.3"},
       {:recon, "~> 2.3"},
+      {:rocksdb, git: "https://gitlab.com/barrel-db/erlang-rocksdb.git"},
       {:sqlitex, "~> 1.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
