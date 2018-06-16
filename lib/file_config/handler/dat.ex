e9b5964e1915c9c064ffbd028bd0da6593d902d3
Jake Morrison <jake@cogini.com>
2018-06-16 10:08:40 +0800

    Make more consistent

--- lib/file_config/handler/dat.ex
+++ lib/file_config/handler/dat.ex
@@ -5,15 +5,17 @@ defmodule FileConfig.Handler.Dat do
 
   alias FileConfig.Loader
 
+  # @impl true
   @spec lookup(Loader.table_state, term) :: term
-  def lookup(%{id: tid}, key) do
+  def lookup(%{id: tid, name: name, data_parser: data_parser}, key) do
     case :ets.lookup(tid, key) do
       [] -> :undefined
       [{^key, value}] ->
-        {:ok, value}
+        {:ok, data_parser.parse_value(name, key, value)}
     end
   end
 
+  # @impl true
   @spec load_update(Loader.name, Loader.update, :ets.tid) :: Loader.table_state
   def load_update(name, update, tid) do
     # Assume updated files contain all records
@@ -26,6 +28,12 @@ defmodule FileConfig.Handler.Dat do
     %{name: name, id: tid, mod: update.mod, handler: __MODULE__}
   end
 
+  # @impl true
+  @spec insert_records(:ets.tab, tuple() | [tuple()]) :: true
+  def insert_records(tid, records) do
+    :ets.insert(tid, records)
+  end
+
   @spec parse_file(Path.t, :ets.tab, map) :: {:ok, non_neg_integer}
   def parse_file(path, tid, _config) do
     {:ok, fh} = :file.open(path, [:read])
@@ -35,13 +43,14 @@ defmodule FileConfig.Handler.Dat do
   end
 
   @spec decode(:file.io_device(), :ets.tid, non_neg_integer) :: {:ok, non_neg_integer}
-  def decode(fh, tid, count) do
+  defp decode(fh, tid, count) do
     case :file.read_line(fh) do
       {:ok, line} ->
         case :re.run(line, '^\s*$|^//.*$') do # TODO: regex string
           :nomatch ->
             split = :re.split(line, "[\.|\n]", [:trim, {:return, :binary}])
             key = :lists.reverse(split)
+            # TODO: convert to Jason
             value = :jsx.encode(Enum.join(split, "."))
             :ets.insert(tid, [{key, value}])
             decode(fh, tid, count + 1)
