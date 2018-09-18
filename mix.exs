20104017135c7f1e1b16444d75a2f5419d07eba2
Jake Morrison <jake@cogini.com>
2018-09-18 17:20:29 +0800

    Upgrade sqlitex

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule FileConfig.Mixfile do
       {:nimble_csv, "~> 0.3"},
       {:recon, "~> 2.3"},
       # {:rocksdb, git: "https://gitlab.com/barrel-db/erlang-rocksdb.git"},
-      {:sqlitex, "~> 1.3"}
+      {:sqlitex, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
