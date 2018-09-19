4266c279379defcaf4f0d9686e094901b0319a64
Jake Morrison <jake@cogini.com>
2018-09-19 08:08:01 +0800

    Configure in application

--- lib/file_config/application.ex
+++ lib/file_config/application.ex
@@ -1,13 +1,19 @@
 defmodule FileConfig.Application do
   @moduledoc false
+  @app :file_config
 
   use Application
 
   def start(_type, _args) do
+
     children = [
       {FileConfig.EventProducer, []},
       # {FileConfig.EventConsumer, []},
-      {FileConfig.Loader, []},
+      {FileConfig.Loader, [
+        files: Application.get_env(@app, :files, []),
+        data_dirs: Application.get_env(@app, :data_dirs, []),
+        check_delay: Application.get_env(@app, :check_delay, 5000)
+      ]},
     ]
 
     opts = [strategy: :one_for_one, name: FileConfig.Supervisor]
