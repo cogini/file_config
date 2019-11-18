54d3ef923b891cc8b0569409f7273978bfc334d4
Jake Morrison <jake@cogini.com>
2019-11-18 18:45:06 +0800

    Remove lager

--- mix.exs
+++ mix.exs
@@ -33,7 +33,6 @@ defmodule FileConfig.MixProject do
       {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
       {:gen_stage, "~> 0.14"},
       {:jason, "~> 1.0"},
-      # {:lager, github: "basho/lager", override: true},
       {:recon, "~> 2.3"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
