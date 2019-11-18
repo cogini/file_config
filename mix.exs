ca93b95f37c3f589ee599bd50aa61069aff160fb
Jake Morrison <jake@cogini.com>
2019-11-18 19:15:55 +0800

    Add hex metadata

--- mix.exs
+++ mix.exs
@@ -1,12 +1,20 @@
 defmodule FileConfig.MixProject do
   use Mix.Project
 
+  @github "https://github.com/cogini/file_config"
+
   def project do
     [
       app: :file_config,
       version: "0.1.0",
       elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      source_url: @github,
+      homepage_url: @github,
       dialyzer: [
         # plt_add_deps: :project,
         # plt_add_apps: [:ssl, :mnesia, :compiler, :xmerl, :inets, :disk_log],
@@ -15,7 +23,10 @@ defmodule FileConfig.MixProject do
         # flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
         # ignore_warnings: "dialyzer.ignore-warnings"
       ],
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
     ]
   end
 
@@ -27,15 +38,45 @@ defmodule FileConfig.MixProject do
     ]
   end
 
+  # defp extra_applications(:test), do: []
+  # defp extra_applications(_),     do: []
+
+  # Specifies which paths to compile per environment
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
       {:gen_stage, "~> 0.14"},
       {:jason, "~> 1.0"},
       {:recon, "~> 2.3"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.12.0", only: [:dev, :test], runtime: false},
     ]
   end
+
+  defp description do
+    "Dynamically load config files into ETS tables."
+  end
+
+  defp package do
+    [
+      maintainers: ["Jake Morrison"],
+      licenses: ["Mozilla Public License 2.0"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: @github,
+      extras: ["README.md"]
+    ]
+  end
+
 end
