8d6c52031430c7be9db975ae3fb696d5f6b099bf
Jake Morrison <jake@cogini.com>
2021-06-17 15:35:02 -0500

    Clean up

--- mix.exs
+++ mix.exs
@@ -55,8 +55,6 @@ defmodule FileConfig.MixProject do
       {:gen_stage, "~> 0.14"},
       {:jason, "~> 1.0"},
       {:recon, "~> 2.3"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 
