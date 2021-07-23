1b2bf080319b228edafd4db31bf76ef538699a79
Jake Morrison <jake@cogini.com>
2021-07-22 21:25:08 -0500

    Formatting

--- lib/file_config/event_producer.ex
+++ lib/file_config/event_producer.ex
@@ -3,24 +3,29 @@ defmodule FileConfig.EventProducer do
 
   use GenStage
 
-  @doc "Start the broadcaster"
-  def start_link(state) do
-    GenStage.start_link(__MODULE__, state, name: __MODULE__)
+  @doc "Start broadcaster"
+  def start_link(args) do
+    GenStage.start_link(__MODULE__, args, name: __MODULE__)
   end
 
-  @doc "Send an event and return only after the event is dispatched"
+  @doc "Send event and return only after it is dispatched"
   def sync_notify(event, timeout \\ 5000) do
     GenStage.call(__MODULE__, {:notify, event}, timeout)
   end
 
-  def init(state) do
-    {:producer, state, dispatcher: GenStage.BroadcastDispatcher}
+  # Callbacks
+
+  @impl true
+  def init(args) do
+    {:producer, args, dispatcher: GenStage.BroadcastDispatcher}
   end
 
+  @impl true
   def handle_call({:notify, event}, _from, state) do
     {:reply, :ok, [event], state} # Dispatch immediately
   end
 
+  @impl true
   def handle_demand(_demand, state) do
     {:noreply, [], state} # We don't care about the demand
   end
