69f8952474b2cc0270e946c89aa7ac3aafc462cd
Jake Morrison <jake@cogini.com>
2021-07-22 21:14:35 -0500

    Formatting and docs; add JSON extension

--- lib/file_config/loader.ex
+++ lib/file_config/loader.ex
@@ -1,6 +1,6 @@
 defmodule FileConfig.Loader do
   @moduledoc "Load files"
-  @extensions [".bert", ".csv", ".dat", ".log"]
+  @extensions [".bert", ".csv", ".dat", ".log", ".json"]
 
   use GenServer
   require Logger
@@ -18,33 +18,51 @@ defmodule FileConfig.Loader do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
-  def init(config) do
+  @impl true
+  def init(args) do
     Process.flag(:trap_exit, true) # Die gracefully
     __MODULE__ = :ets.new(__MODULE__, [:set, :public, :named_table, {:read_concurrency, true}])
 
-    data_dirs = config[:data_dirs] || []
-    file_configs = process_file_configs(config[:files] || [])
-    check_delay = config[:check_delay] || 5000
+    # Directories to look for files in
+    data_dirs = args[:data_dirs] || []
+
+    file_configs = process_file_configs(args[:files] || [])
+
+    # How often to check for new files, in ms
+    check_delay = args[:check_delay] || 5000
 
     {old_tables, new_files} = check_files(%{}, %{data_dirs: data_dirs, file_configs: file_configs}, true)
 
     free_binary_memory()
-    {:ok, %{ref: :erlang.start_timer(check_delay, self(), :reload),
-      old_tables: old_tables, files: new_files,
-      file_configs: file_configs, data_dirs: data_dirs, check_delay: check_delay}}
+
+    state = %{
+      ref: :erlang.start_timer(check_delay, self(), :reload),
+      old_tables: old_tables,
+      files: new_files,
+      file_configs: file_configs,
+      data_dirs: data_dirs,
+      check_delay: check_delay,
+    }
+
+    {:ok, state}
   end
 
-  @spec handle_info(term, map) :: {:noreply, map}
+  @impl true
+  @spec handle_info(term(), map()) :: {:noreply, map()}
   def handle_info({:timeout, ref, :reload}, state) do
     %{ref: ^ref, files: files, old_tables: old_old_tables} = state # TODO check ref matching
     {old_tables, new_files} = check_files(files, state)
     delete_tables(old_old_tables)
 
     free_binary_memory()
+
     {:noreply, %{state | ref: :erlang.start_timer(state.check_delay, self(), :reload),
       files: new_files, old_tables: old_tables}}
   end
-  def handle_info(_event, state) do
+
+  def handle_info(msg, state) do
+    Logger.debug("Unexpected message: #{inspect(msg)}")
+
     {:noreply, state}
   end
 
@@ -76,16 +94,24 @@ defmodule FileConfig.Loader do
     {old_tables, new_files}
   end
 
-  @doc "Process file configs to set defaults"
-  @spec process_file_configs(list({name, map})) :: list(file_config)
+  @doc "Set config defaults"
+  @spec process_file_configs(list({name(), map()})) :: list(file_config())
   def process_file_configs(files) do
     for {config_name, config} <- files do
       Logger.info("Loading #{config_name} #{inspect config}")
+
+      # Name of table
       name = config[:name] || config_name
+
+      # Pattern matching input files
       file = config.file
-      format = config[:format] || ext_to_format(Path.extname(file))
-      handler = config[:handler] || format_to_handler(format)
       regex = Regex.compile!("/#{file}$")
+
+      format = config[:format] || ext_to_format(Path.extname(file))
+
+      # Module to handle file
+      handler = config[:handler] || format_to_handler(format)
+
       Map.merge(config, %{
         name: name,
         format: format,
@@ -95,7 +121,7 @@ defmodule FileConfig.Loader do
     end
   end
 
-  @doc "Look for files in data dirs"
+  @doc "Find files matching pattern."
   @spec get_files(list(Path.t()), list(file_config)) :: files()
   def get_files(data_dirs, file_configs, init \\ false) do
     path_configs =
@@ -104,6 +130,9 @@ defmodule FileConfig.Loader do
         config <- file_configs,
         Regex.match?(config.regex, path), do: {path, config}
 
+    # Skip files marked as async on initial startup pass.
+    # This allows the program to start up more quickly and
+    # handle reuests, processing the files on the next scheduled run.
     is_async =
       fn
         {path, %{async: true}} ->
@@ -113,7 +142,8 @@ defmodule FileConfig.Loader do
           else
             false
           end
-        _ -> false
+        _ ->
+          false
       end
 
     path_configs = Enum.reject(path_configs, is_async)
@@ -123,16 +153,16 @@ defmodule FileConfig.Loader do
         {:ok, stat} = File.stat(path),
         stat.size > 0, do: {path, config, %{mod: stat.mtime}}
 
-    # for {path, config, mtime} <- files do
-    #   Logger.warning("file: #{inspect(path)} #{inspect(config)} #{inspect(mtime)}")
-    # end
+    for {path, config, mtime} <- files do
+      Logger.debug("file: #{inspect(path)} #{inspect(config)} #{inspect(mtime)}")
+    end
 
     files
     |> Enum.reduce(%{}, &group_by_name/2)
     |> Enum.reduce(%{}, &sort_by_mod/2)
   end
 
-  @doc "List files in dir with config file extensions"
+  @doc "List files in dir matching configured file extensions."
   @spec list_files(Path.t()) :: [Path.t()]
   def list_files(dir) do
     with {:ok, _stat} <- File.stat(dir),
@@ -151,6 +181,7 @@ defmodule FileConfig.Loader do
   end
 
   @doc "Collect multiple files for the same name"
+  @spec group_by_name({Path.t(), map(), map()}, map()) :: map()
   def group_by_name({path, %{name: name} = config, state}, acc) do
     case Map.fetch(acc, name) do
       :error ->
@@ -164,7 +195,7 @@ defmodule FileConfig.Loader do
   @spec sort_by_mod({name(), update()}, map()) :: map()
   def sort_by_mod({name, v}, acc) do
     # Sort files by modification time (newer to older)
-    files = Enum.sort(v.files, fn({_, %{mod: a}}, {_, %{mod: b}}) -> a >= b end)
+    files = Enum.sort(v.files, fn {_, %{mod: a}}, {_, %{mod: b}} -> a >= b end)
     {_path, %{mod: mod}} = hd(files)
     Map.put(acc, name, Map.merge(v, %{files: files, mod: mod}))
   end
@@ -172,25 +203,27 @@ defmodule FileConfig.Loader do
   @doc "Determine if files have changed since last run"
   @spec get_changed_files(files(), files()) :: files()
   def get_changed_files(new_files, old_files) do
-    Enum.reduce(new_files, %{}, fn({name, v}, acc) ->
-      case Map.fetch(old_files, name) do
-        :error -> # New file
-          Map.put(acc, name, v)
-        {:ok, %{mod: prev_mod}} -> # Existing file
+    Enum.reduce(new_files, %{},
+      fn {name, v}, acc ->
+        case Map.fetch(old_files, name) do
+          :error -> # New file
+            Map.put(acc, name, v)
+          {:ok, %{mod: prev_mod}} -> # Existing file
           # Get files that have been modified since last time
-          mod_files = for {_p, %{mod: mod}} = f <- v.files, mod > prev_mod, do: f
-          if length(mod_files) > 0 do
+            mod_files = for {_p, %{mod: mod}} = f <- v.files, mod > prev_mod, do: f
+            # TODO: check for empty not length
+            if length(mod_files) > 0 do
             # Map.put(acc, name, %{v | files: mod_files}) # only modified files
-            Map.put(acc, name, v) # keep all files
-          else
-            acc
-          end
-      end
-    end)
+              Map.put(acc, name, v) # keep all files
+            else
+              acc
+            end
+        end
+      end)
   end
 
   @doc "Load data from changed files"
-  @spec process_changed_files(files) :: list(table_state)
+  @spec process_changed_files(files()) :: list(table_state())
   def process_changed_files(changed_files) do
     for {name, update} <- changed_files do
       config = update.config
@@ -258,23 +291,25 @@ defmodule FileConfig.Loader do
   end
 
   @doc "Notify subscribers about updates"
-  @spec notify_update([map]) :: :ok
+  @spec notify_update([map()]) :: :ok
   def notify_update(tables) do
     for %{name: name} <- tables do
       # Logger.debug("notify_update: #{inspect name}")
       FileConfig.EventProducer.sync_notify({:load, name})
     end
+
     :ok
   end
 
   @doc "Get format from extension"
-  @spec ext_to_format(Path.t) :: atom
+  @spec ext_to_format(Path.t()) :: atom()
   def ext_to_format(ext)
   def ext_to_format(".bert"), do: :bert
   def ext_to_format(".csv"), do: :csv
   def ext_to_format(".dat"), do: :dat
+  def ext_to_format(".json"), do: :json
 
-  @spec format_to_handler(atom) :: module
+  @spec format_to_handler(atom()) :: module()
   def format_to_handler(ext)
   def format_to_handler(:bert), do: FileConfig.Handler.Bert
   def format_to_handler(:csv), do: FileConfig.Handler.Csv
@@ -287,10 +322,10 @@ defmodule FileConfig.Loader do
   # defp is_async({_name, %{config: %{async: true}}}), do: true
   # defp is_async(_), do: false
 
+  # Manually trigger garbage collection to clear refc binary memory
   def free_binary_memory do
     {:binary_memory, binary_memory} = :recon.info(self(), :binary_memory)
     if binary_memory > 50_000_000 do
-      # Manually trigger garbage collection to clear refc binary memory
       Logger.debug("Forcing garbage collection")
       :erlang.garbage_collect(self())
     end
