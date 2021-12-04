81e0d9681d5589c87667c89e8f221a1945c7282f
Jake Morrison <jake@cogini.com>
2021-12-04 17:09:25 +0800

    mix format

--- lib/file_config/application.ex
+++ lib/file_config/application.ex
@@ -6,19 +6,19 @@ defmodule FileConfig.Application do
 
   @impl true
   def start(_type, _args) do
-
     # {:ok, _} = Application.ensure_all_started(:file_config_rocksdb, :permanent)
     # {:ok, _} = Application.ensure_all_started(:file_config_sqlite, :permanent)
 
     children = [
       {FileConfig.EventProducer, []},
       # {FileConfig.EventConsumer, []},
-      {FileConfig.Loader, [
-        state_dir: Application.get_env(@app, :state_dir),
-        files: Application.get_env(@app, :files, []),
-        data_dirs: Application.get_env(@app, :data_dirs, []),
-        check_delay: Application.get_env(@app, :check_delay, 5000)
-      ]},
+      {FileConfig.Loader,
+       [
+         state_dir: Application.get_env(@app, :state_dir),
+         files: Application.get_env(@app, :files, []),
+         data_dirs: Application.get_env(@app, :data_dirs, []),
+         check_delay: Application.get_env(@app, :check_delay, 5000)
+       ]}
     ]
 
     opts = [strategy: :one_for_one, name: FileConfig.Supervisor]
