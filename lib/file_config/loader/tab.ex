937b5cb8824e91611c758a2b54e7edf560c456bc
Jake Morrison <jake@cogini.com>
2021-12-04 17:10:12 +0800

    mix format

--- lib/file_config/loader/tab.ex
+++ lib/file_config/loader/tab.ex
@@ -2,14 +2,23 @@ defmodule FileConfig.Loader.Tab do
   @moduledoc "Table"
 
   require Record
-  Record.defrecord :tab, name: nil, id: nil, timestamp: {{0, 0, 0}, {0, 0, 0}}, type: :ets, db_path: ""
 
-  @type tab :: record(:tab,
-                      name: atom,
-                      id: :ets.tid,
-                      timestamp: :calendar.datetime,
-                      type: :ets | :db,
-                      db_path: :file.filename_all)
+  Record.defrecord(:tab,
+    name: nil,
+    id: nil,
+    timestamp: {{0, 0, 0}, {0, 0, 0}},
+    type: :ets,
+    db_path: ""
+  )
+
+  @type tab ::
+          record(:tab,
+            name: atom,
+            id: :ets.tid(),
+            timestamp: :calendar.datetime(),
+            type: :ets | :db,
+            db_path: :file.filename_all()
+          )
 
   # defstruct [
   #   name: nil,
